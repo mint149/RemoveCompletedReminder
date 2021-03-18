8b95a6c7d6ab1c08d1979af9a1898167972298ab
Mint <skylove.exe@gmail.com>
2021-03-18 10:02:18 +0900

    Initial Commit

---
+++ RemoveCompletedReminder/RemoveCompletedReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoveCompletedReminderApp.swift
+//  RemoveCompletedReminder
+//
+//  Created by hato on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct RemoveCompletedReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
